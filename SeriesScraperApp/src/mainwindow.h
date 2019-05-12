martinLutz1/SeriesScraper

commit 4abebef962a41569687464ac000aec384e178e8c

Support multiple test classes
Use the ADD_TEST macro to unit test more than one class.

Additionally:
- MainWindow: Replace some c-style casts by static_cast<>().
- Remove "Series information" text in series  information sidebar.
- EpisodeName: Return references when possible.
- FileTypeHandler: Do not remove out of bound indices.

SeriesScraperApp/src/mainwindow.h
@@ -14,6 +14,7 @@
 
 #include "customshadoweffect.h"
 #include "QProgressIndicator.h"
+#include "lineedit.h"
 #include "keypresseater.h"
 #include "aboutdialog.h"
 #include "message.h"
@@ -21,8 +22,9 @@
 #include "settings.h" // DirectorySelector enum.
 #include "directoryparser.h" // NUMBER_PATH_STRUCTURE_COMBOBOXES
 
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -30,7 +32,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     enum class StatusMessageType { error, success, info };
 

DeploymentFiles/mac/Info.plist
@@ -18,5 +18,7 @@
 	<string>com.yourcompany.SeriesScraper</string>
 	<key>CFBundleVersion</key>
 	<string>0.7.0</string>
+	<key>NSRequiresAquaSystemAppearance</key>
+	<string>true</string>
 </dict>
 </plist>

SeriesScraperApp/SeriesScraperApp.pro
@@ -55,10 +55,11 @@ macx {
     debug {
         #CONFIG -= app_bundle
     }
-    QMAKE_MAC_SDK = macosx10.13
+    QMAKE_MAC_SDK = macosx10.14
     ICON = $$PWD/images/app_logo/macIcon.icns
 
-    QMAKE_POST_LINK += ~/Qt5.10.0/5.10.0/clang_64/bin/macdeployqt $$OUT_PWD/SeriesScraperApp.app;
+    QMAKE_POST_LINK += cd ~/Qt/5.12.3/;
+    QMAKE_POST_LINK += ~/Qt/5.12.3/clang_64/bin/macdeployqt $$OUT_PWD/SeriesScraperApp.app;
     QMAKE_POST_LINK += cp -Rf $$PWD/../DeploymentFiles/general/* $$OUT_PWD/SeriesScraperApp.app/Contents/MacOs;
     QMAKE_POST_LINK += cp -Rf $$PWD/../DeploymentFiles/mac/Info.plist $$OUT_PWD/SeriesScraperApp.app/Contents;
 }

SeriesScraperApp/SeriesScraperAppSrc.pri
@@ -1,63 +1,0 @@
-SOURCES += \
-    directoryparser.cpp \
-    languagedata.cpp \
-    controller.cpp \
-    seriesdata.cpp \
-    message.cpp \
-    filerenamer.cpp \
-    nameschemeparser.cpp \
-    nameschemehandler.cpp \
-    omdbseriesparser.cpp \
-    tmdbseriesparser.cpp \
-    jsonscraper.cpp \
-    error.cpp \
-    settings.cpp \
-    jsonloader.cpp \
-    seriesparser.cpp \
-    seriesfilesaver.cpp \
-    customshadoweffect.cpp \
-    keypresseater.cpp \
-    rawloader.cpp \
-    filetypehandler.cpp \
-    serieslanguagehandler.cpp \
-    interfacelanguagehandler.cpp \
-    baseseriesparser.cpp \
-    basefiledownloader.cpp \
-    tvdbseriesparser.cpp \
-    directoryhandler.cpp \
-    QProgressIndicator.cpp \
-    genericfiledownloader.cpp \
-    episodename.cpp \
-    episodenamehandler.cpp
-
-HEADERS  += \
-    directoryparser.h \
-    languagedata.h \
-    controller.h \
-    seriesdata.h \
-    message.h \
-    filerenamer.h \
-    nameschemeparser.h \
-    nameschemehandler.h \
-    omdbseriesparser.h \
-    tmdbseriesparser.h \
-    jsonscraper.h \
-    error.h \
-    settings.h \
-    jsonloader.h \
-    seriesparser.h \
-    seriesfilesaver.h \
-    customshadoweffect.h \
-    keypresseater.h \
-    rawloader.h \
-    filetypehandler.h \
-    serieslanguagehandler.h \
-    interfacelanguagehandler.h \
-    baseseriesparser.h \
-    basefiledownloader.h \
-    tvdbseriesparser.h \
-    directoryhandler.h \
-    QProgressIndicator.h \
-    genericfiledownloader.h \
-    episodename.h \
-    episodenamehandler.h

SeriesScraperApp/forms/mainwindow.ui
@@ -231,7 +231,7 @@
             </size>
            </property>
            <property name="title">
-            <string>Series information</string>
+            <string/>
            </property>
            <property name="alignment">
             <set>Qt::AlignLeading|Qt::AlignLeft|Qt::AlignVCenter</set>

SeriesScraperApp/src/mainwindow.cpp
@@ -67,7 +67,7 @@ MainWindow::~MainWindow()
 
     Message msgSetDirectorySelector;
     msgSetDirectorySelector.type = Message::Type::view_setDirectorySelector_controller;
-    msgSetDirectorySelector.data[0].i = (int)directorySelector;
+    msgSetDirectorySelector.data[0].i = static_cast<int>(directorySelector);
     emit(sendMessage(msgSetDirectorySelector));
 
     delete ui;
@@ -355,9 +355,9 @@ void MainWindow::resizeEvent(QResizeEvent *event)
 
     if (seriesInformationEnabled)
     {
-        episodeTableWidth = 0.80 * (windowWidth - 2 * UNIVERSAL_SPACER);
-        renameProgressWidgetX = (episodeTableWidth - renameProgressWidgetWidth) / 2;
-        int additionalInfoWidth = 0.20 * (windowWidth - 2 * UNIVERSAL_SPACER);
+        episodeTableWidth = static_cast<int>(0.80 * (windowWidth - 2 * UNIVERSAL_SPACER));
+        renameProgressWidgetX = static_cast<int>((episodeTableWidth - renameProgressWidgetWidth) / 2);
+        int additionalInfoWidth = static_cast<int>(0.20 * (windowWidth - 2 * UNIVERSAL_SPACER));
         int additionalInfoHeight = episodeTableHeight;
 
         if (additionalInfoWidth > 300)
@@ -366,7 +366,7 @@ void MainWindow::resizeEvent(QResizeEvent *event)
             episodeTableWidth = windowWidth - 2 * UNIVERSAL_SPACER - additionalInfoWidth;
         }
         int imageLabelWidth = additionalInfoWidth - 2 * UNIVERSAL_SPACER;
-        int imageLabelHeight = episodeTableHeight * 0.6;
+        int imageLabelHeight = static_cast<int>(episodeTableHeight * 0.6);
         int additionalInfoX = episodeTableX + episodeTableWidth - 1;
         int additionalInfoY = UNIVERSAL_SPACER;
 
@@ -385,8 +385,8 @@ void MainWindow::resizeEvent(QResizeEvent *event)
 
     //Resize
     ui->episodeNameTable->setFixedSize(episodeTableWidth, episodeTableHeight);
-    ui->episodeNameTable->setColumnWidth(0, episodeTableWidth*0.45);
-    ui->episodeNameTable->setColumnWidth(1, episodeTableWidth*0.47);
+    ui->episodeNameTable->setColumnWidth(0, static_cast<int>(episodeTableWidth * 0.45));
+    ui->episodeNameTable->setColumnWidth(1, static_cast<int>(episodeTableWidth * 0.47));
     ui->additionalInfoScrollArea->setFixedHeight(episodeTableHeight);
     ui->centralControlElementWidget->setFixedWidth(controlWidth);
 
@@ -400,12 +400,12 @@ void MainWindow::updateView(EpisodeNames* episodeNames, bool atLeastOneSideEmpty
 
     const auto tableSize = episodeNames->size();
     setAmountSeasons(amountSeasons);
-    ui->episodeNameTable->setRowCount(tableSize);
+    ui->episodeNameTable->setRowCount(static_cast<int>(tableSize));
 
     // Fill table
-    for (auto row = 0; row < tableSize; row++)
+    for (std::size_t row = 0; row < tableSize; row++)
     {
-        updateRow(row, episodeNames->at(row), atLeastOneSideEmpty);
+        updateRow(static_cast<int>(row), episodeNames->at(row), atLeastOneSideEmpty);
     }
 
     QObject::connect(ui->episodeNameTable, SIGNAL(cellChanged(int,int)), this, SLOT(onTableEntryChanged(int,int)));
@@ -426,11 +426,11 @@ void MainWindow::updateDirectoryWidget(std::vector<QStringList> pathStructure, b
     directoryEntriesMenu->addAction(QIcon(":/ui_icons/folder_video.png"), openThisFolderText);
 
     // Fill comboboxes
-    for (int i = 0; i < int(pathStructure.size() - 2); i++)
+    for (std::size_t i = 0; i < (pathStructure.size() - 2); i++)
     {
-        for (int j = 0; j < pathStructure.at(i + 1).size(); j++)
+        for (std::size_t j = 0; j < static_cast<std::size_t>(pathStructure.at(i + 1).size()); j++)
         {
-            if (containsRoot && (int(pathStructure.size() - 3) == i))
+            if (containsRoot && ((pathStructure.size() - 3) == i))
             {
                 pathStructureComboBoxList[i]->addItem(QIcon(":/ui_icons/hdd.png"), pathStructure.at(i + 1).at(j));
             }
@@ -733,7 +733,6 @@ void MainWindow::changeLocalization(QStringList translationList)
     switchDirectorySelectorAction->setText(translationList.at((int)LanguageData::Translate::switchDirectoryView));
     reloadDirectoryAction->setText(translationList.at((int)LanguageData::Translate::reload));
     // Info sidebar
-    ui->infoGroupBox->setTitle(translationList.at((int)LanguageData::Translate::seriesInfo));
     ui->airDateInfoLabel->setText(translationList.at((int)LanguageData::Translate::airDate) + ":");
     ui->seasonInfoLabel->setText(translationList.at((int)LanguageData::Translate::season) + ":");
     ui->totalEpisodesInfoLabel->setText(translationList.at((int)LanguageData::Translate::episodeNumber) + ":");
@@ -1317,7 +1316,7 @@ void MainWindow::updateRow(int row, EpisodeName& episodeName, bool noColorizatio
     const auto oldFileName = episodeName.getOldName();
     const auto newFileName = episodeName.getNewName();
 
-    const auto& table = ui->episodeNameTable;
+    auto& table = ui->episodeNameTable;
     auto leftSide = table->item(row, 0);
     auto rightSide = table->item(row, 1);
     const auto tableSize = table->rowCount();

SeriesScraperLib/src/episodename.cpp
@@ -12,7 +12,7 @@ EpisodeName::EpisodeName(
 {
 }
 
-const QString EpisodeName::getOldName() const
+const QString& EpisodeName::getOldName() const
 {
     return oldName;
 }
@@ -23,7 +23,7 @@ const QString EpisodeName::getOldNameWithSuffix() const
     return oldNameWithSuffix;
 }
 
-const QString EpisodeName::getNewName() const
+const QString& EpisodeName::getNewName() const
 {
     return newName;
 }
@@ -34,7 +34,7 @@ const QString EpisodeName::getNewNameWithSuffix() const
     return newNameWithSuffix;
 }
 
-const QString EpisodeName::getFileType() const
+const QString& EpisodeName::getFileType() const
 {
     return fileType;
 }

SeriesScraperLib/src/episodename.h
@@ -16,16 +16,16 @@ public:
 
 public:
     EpisodeName(
-            QString oldName = "",
-            QString newName = "",
-            QString fileType = "",
-            Position position = Position::unsure);
+        QString oldName = "",
+        QString newName = "",
+        QString fileType = "",
+        Position position = Position::unsure);
 
-    const QString getOldName() const;
+    const QString& getOldName() const;
     const QString getOldNameWithSuffix() const;
-    const QString getNewName() const;
+    const QString& getNewName() const;
     const QString getNewNameWithSuffix() const;
-    const QString getFileType() const;
+    const QString& getFileType() const;
     bool newAndOldAreEqual() const;
     bool isPositionDetermined() const;
     bool atLeastOneNameEmpty() const;

commit 350c8d05b8fd33a3a886bc39ff4bf79ac747799f

Remove include for non existing file.
Fixes: 4abebef962a41569687464ac000aec384e178e8c

SeriesScraperApp/src/mainwindow.h
@@ -3,7 +3,6 @@
 
 #define APPLICATIONNAME "SeriesScraper"
 
-
 #include <QMainWindow>
 #include <QDir>
 #include <QLabel>
@@ -14,7 +13,6 @@
 
 #include "customshadoweffect.h"
 #include "QProgressIndicator.h"
-#include "lineedit.h"
 #include "keypresseater.h"
 #include "aboutdialog.h"
 #include "message.h"